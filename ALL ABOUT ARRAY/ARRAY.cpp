d91d6becbcb7a9616e93fae162475654257d9815
HARSH VARDHAN TIWARI <hvstatuswala@gmail.com>
2024-09-26 22:17:44 +0530

    ARRAY.cpp
    PROGRAM TO DEFINE AND CREATE A DYNAMIC ARRAY IN C PROGRAMING LANGAUGE

--- ALL ABOUT ARRAY/ARRAY.cpp
+++ ALL ABOUT ARRAY/ARRAY.cpp
@@ -1,1 +1,18 @@
+// PROGRAM TO MAKE A DYNAMIC ARRAY 
+#include <stdio.h>
 
+int main() {
+   int size;
+  
+   printf("ENTRE SIZE OF ARRAY \n");
+   scanf("%d",&size);
+  
+   int array[size];
+  
+   for (int i=0; i<size; i++)
+   {
+       printf("Entre %d element of array\n",i+1);
+       scanf("%d",&array[i]);
+   }
+    return 0;
+}
