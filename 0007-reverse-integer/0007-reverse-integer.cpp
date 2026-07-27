7f8b2cf247a379eaaaf08fedd9833559e2805f01
HARSH VARDHAN TIWARI <hvstatuswala@gmail.com>
2026-07-27 20:45:02 +0530

    Time: 0 ms (100%), Space: 8.5 MB (54.72%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.cpp
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
     int reverse(int x) {
-        int rev = 0;
-
-        while (x != 0) {
-            int dig = x % 10;
-
-            if (rev > 214748364 || (rev == 214748364 && dig > 7))
+        //if(x > 2e31 -1 || x < -2e31) return 0;
+        int rev =0;
+        
+        while(x != 0){
+            int dig = x%10;
+            if(rev > 214748364 || (rev == 214748364 && dig ==7)){
                 return 0;
-
-            if (rev < -214748364 || (rev == -214748364 && dig < -8))
+            }
+            if(rev < -214748364 || (rev == -214748364 && dig == -8)){
                 return 0;
-
+            }
             rev = rev * 10 + dig;
-            x /= 10;
+            x = x / 10;
         }
-
         return rev;
     }
 };
+
