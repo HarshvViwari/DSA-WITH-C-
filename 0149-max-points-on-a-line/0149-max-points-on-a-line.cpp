95c24f405f31b747c7d3e5b8452e1debd19b62a8
HARSH VARDHAN TIWARI <hvstatuswala@gmail.com>
2026-07-29 14:22:56 +0530

    Time: 19 ms (79.83%), Space: 15.5 MB (74.34%) - LeetHub

--- 0149-max-points-on-a-line/0149-max-points-on-a-line.cpp
+++ 0149-max-points-on-a-line/0149-max-points-on-a-line.cpp
@@ -9,7 +9,7 @@ public:
             for(int j=i+1;j<n;j++){
                 
                 double x = (double)(pt[j][1]-pt[i][1])/(double)(pt[j][0]-pt[i][0]);
-                if(pt[j][1]-pt[i][1]<0 &&(pt[j][0]-pt[i][0])==0 )
+                if(pt[j][1]-pt[i][1]< 0 &&(pt[j][0]-pt[i][0])==0 )
                  mp[abs(x)]++; 
                 else
                 mp[x]++;
