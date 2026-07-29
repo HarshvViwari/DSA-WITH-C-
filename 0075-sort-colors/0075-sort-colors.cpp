3100e854601cb046a54561efbb4f1b719a1ae984
HARSH VARDHAN TIWARI <hvstatuswala@gmail.com>
2026-07-29 09:40:58 +0530

    Time: 0 ms (100%), Space: 11.4 MB (98.21%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,20 +1,36 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        int low = 0, mid = 0, high = nums.size()-1;
-        while(mid <= high){
-            if(nums[mid] == 0){
-                swap(nums[low], nums[mid]);
-                low++;
-                mid++;
-            }
-            else if(nums[mid] == 1){
-                mid++;
-            }
-            else{
-                swap(nums[mid], nums[high]);
-                high--;
+
+        int a=0,b=0,c=0;
+
+        for(int i : nums){
+            if(i == 0){
+                nums[c++] = 2;
+                nums[b++] = 1;
+                nums[a++] = 0;
+            }if(i == 1){
+                nums[c++] = 2;
+                nums[b++] = 1;
+            }if(i == 2){
+                nums[c++] = 2;
             }
         }
-    }
+
+        // int low = 0, mid = 0, high = nums.size()-1;
+        // while(mid <= high){
+        //     if(nums[mid] == 0){
+        //         swap(nums[low], nums[mid]);
+        //         low++;
+        //         mid++;
+        //     }
+        //     else if(nums[mid] == 1){
+        //         mid++;
+        //     }
+        //     else{
+        //         swap(nums[mid], nums[high]);
+        //         high--;
+        //     }
+        }
+    
 };
