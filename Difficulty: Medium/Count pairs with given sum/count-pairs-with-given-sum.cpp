628734156d0d4412be1950a96853cbbd7326bef7
HARSH VARDHAN TIWARI <hvstatuswala@gmail.com>
2026-08-12 10:03:35 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Count pairs with given sum/count-pairs-with-given-sum.cpp
@@ -0,0 +1,22 @@
+class Solution {
+  public:
+    int countPairs(vector<int> &arr, int target) {
+        // Code here
+        map<int,int>mp;
+        
+        int ans =0;
+        
+        for(int x : arr){
+            int need = target - x;
+            
+            if(mp.find(need) != mp.end()){
+                ans += mp[need];
+            }
+            
+            mp[x]++;
+        }
+        
+        return ans;
+        
+    }
+};
